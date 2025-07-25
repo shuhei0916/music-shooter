8853f1b918cdb91c62be3eceddc0fd61db1f4378
shuhei0916 <shuheiito.stc@gmail.com>
2025-07-25 13:23:33 +0900

    Refactor gate scene and script; add floating and rotation animations for enhanced visual dynamics

--- gate.gd
+++ gate.gd
@@ -5,6 +5,11 @@ signal player_entered_gate(gate_type, value)
 @export var gate_type: String = "add" # "add", "multiply"
 @export var value: int = 1
 
+var time = 0.0
+const FLOAT_SPEED = 2.0
+const FLOAT_AMOUNT = 0.2
+const ROTATION_SPEED = 1.0
+
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
 	update_label()
@@ -20,7 +25,19 @@ func set_gate_properties(type, val):
 	update_label()
 
 func update_label():
-	var label = $Label3D
+	var label = get_node("Pivot/Label3D")
 	if label:
 		var prefix = "+" if gate_type == "add" else "x"
 		label.text = prefix + str(value)
+
+func _process(delta):
+	time += delta
+	
+	var pivot = get_node("Pivot")
+	if pivot:
+		# Floating animation (up and down)
+		var float_offset = sin(time * FLOAT_SPEED) * FLOAT_AMOUNT
+		pivot.position.y = float_offset
+		
+		# Rotation animation
+		pivot.rotate_y(ROTATION_SPEED * delta)
