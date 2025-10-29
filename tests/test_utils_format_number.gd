498a40199eaa98ac589bcd842a0b654a7e1796ad
shuhei0916 <shuheiito.stc@gmail.com>
2025-10-29 22:26:29 +0900

    test: cover format number suffix for thousands

--- tests/test_utils_format_number.gd
+++ tests/test_utils_format_number.gd
@@ -4,6 +4,12 @@ class_name TestUtilsFormatNumber
 
 func test_999はそのままの文字列を返す():
 	var utils = load("res://utils.gd").new()
-	var result = utils.format_number(999.0)
+	var result = utils.format_number(999)
 	utils.free()
 	assert_eq("999", result)
+
+func test_1000は1ドット0Kを返す():
+	var utils = load("res://utils.gd").new()
+	var result = utils.format_number(1000)
+	utils.free()
+	assert_eq("1.0K", result)
--- utils.gd
+++ utils.gd
@@ -2,22 +2,17 @@ extends Node
 
 # Formats a number into a string with metric suffixes (K, M, B, T)
 func format_number(num):
-	var num_float = float(num)
+	var value = int(num)
 
-	if num_float < 1000.0:
-		if fmod(num_float, 1.0) == 0.0:
-			return str(int(num_float))
-		return "%.1f" % num_float
-	
+	if value < 1000:
+		return str(value)
+
 	var suffixes = ["", "K", "M", "B", "T"] # Kilo, Mega, Giga(Billion), Tera
 	var i = 0
-	
+	var num_float = float(value)
+
 	while num_float >= 1000.0 and i < suffixes.size() - 1:
 		num_float /= 1000.0
 		i += 1
 	
-	# Format to one decimal place if it's not a whole number
-	if fmod(num_float, 1.0) == 0:
-		return str(int(num_float)) + suffixes[i]
-	else:
-		return "%.1f" % num_float + suffixes[i]
+	return "%.1f" % num_float + suffixes[i]
