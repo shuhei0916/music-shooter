9fd1a5a8e2e7d16e62db6c35478606f6c6c06292
shuhei0916 <shuheiito.stc@gmail.com>
2025-11-15 14:16:23 +0900

    弾丸と敵の衝突をenemy.gdで判定するよう修正

--- scenes/characters/enemy/enemy.gd
+++ scenes/characters/enemy/enemy.gd
@@ -9,6 +9,11 @@ func _on_body_entered(body):
 	if body.is_in_group("player"):
 		body._on_enemy_collided(self)
 
+func _on_area_entered(area: Area3D) -> void:
+	if area.is_in_group("bullet"):
+		take_damage(area.damage)
+		area.queue_free()
+
 func set_hp(new_hp):
 	hp = new_hp
 	update_label()
--- scenes/objects/bullet/bullet.gd
+++ scenes/objects/bullet/bullet.gd
@@ -2,14 +2,10 @@ class_name Bullet
 extends Area3D
 
 var speed = 30.0
+@export var damage: int = 1
 
 func _process(delta):
 	position.z -= speed * delta
-
-func _on_area_entered(area):
-	if area.is_in_group("enemy"):
-		area.take_damage(1) # 仮のダメージ値
-	queue_free()
 
 func _on_visible_on_screen_notifier_3d_screen_exited():
 	queue_free()
