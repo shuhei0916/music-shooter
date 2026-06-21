3985df0a0b744d8264dceb6ad7ed73b3e981bc73
shuhei0916 <shuheiito.stc@gmail.com>
2026-06-21 21:27:49 +0900

    feat: Handgunの発射時にマズルフラッシュ（OmniLight3D）を追加する

--- scenes/objects/weapons/handgun/handgun.gd
+++ scenes/objects/weapons/handgun/handgun.gd
@@ -3,6 +3,8 @@ extends "res://scenes/objects/weapons/weapon.gd"
 
 @export var bullet_scene: PackedScene
 
+@onready var _flash: OmniLight3D = $Muzzle/FlashLight
+
 
 func fire() -> void:
 	if bullet_scene == null:
@@ -11,3 +13,10 @@ func fire() -> void:
 	bullet.color = color
 	get_tree().root.add_child(bullet)
 	bullet.global_transform = $Muzzle.global_transform
+	_trigger_flash()
+
+
+func _trigger_flash() -> void:
+	_flash.light_energy = 3.0
+	var tween = create_tween()
+	tween.tween_property(_flash, "light_energy", 0.0, 0.1)
